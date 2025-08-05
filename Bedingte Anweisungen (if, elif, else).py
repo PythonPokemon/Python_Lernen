94c16d002effd158af7a4325031adea59c465113
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-08-05 15:53:00 +0200

    Mein Commit-Text

---
+++ Bedingte Anweisungen (if, elif, else).py
@@ -0,0 +1,8 @@
+zahl = int(input("Gib eine Zahl ein: "))
+
+if zahl > 0:
+    print("Die Zahl ist positiv.")
+elif zahl < 0:
+    print("Die Zahl ist negativ.")
+else:
+    print("Die Zahl ist null.")
---
+++ Tag_1/Einfache Ein- und Ausgabe.py
@@ -0,0 +1,3 @@
+# Eingabe mit input(), Ausgabe mit print()
+name = input("Wie heißt du? ")  # Fragt den Benutzer nach einem Namen
+print("Hallo, " + name + "!")   # Begrüßt den Benutzer
