b1ebccec0a9e95d2e4ba15a3d3a0a993ceb11d20
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-08-06 10:57:24 +0200

    Datei lesen

---
+++ Tag 3/Datei lesen.py
@@ -0,0 +1,12 @@
+""" 
+7. Eingabe- & Ausgabe-Terminologie verstehen
+📌 Was ist Input/Output?
+Input: Der Benutzer gibt etwas ein (Tastatur, Datei, etc.)
+
+Output: Das Programm gibt etwas aus (Bildschirm, Datei)
+"""
+# 💡 Text aus Datei lesen:
+# Datei im Lesemodus öffnen ("r")
+with open("ausgabe.txt", "r") as datei:
+    inhalt = datei.read()
+    print(inhalt)
