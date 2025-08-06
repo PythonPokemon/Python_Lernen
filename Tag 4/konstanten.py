5100f738b93a3ba9c08dfce2521306b9faad92f0
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-08-06 14:07:26 +0200

    update

---
+++ Tag 4/konstanten.py
@@ -0,0 +1,8 @@
+""" 
+Wie schreibt man ein benutzerdefiniertes Modul mit Konstanten?
+"""
+
+PI = 3.14159
+NAME = "Mathe-Modul"    # konstante wierd in Großbuchstaben geschrieben
+
+
---
+++ Tag 4/main2.py
@@ -0,0 +1,11 @@
+""" 
+Wie schreibt man ein benutzerdefiniertes Modul mit Konstanten?
+hat eine referenz zu der datei konstanten.py
+Konstanten sind Variablen, die sich nicht ändern sollen.
+"""
+
+import konstanten
+
+# Zugriff auf die Konstanten
+print(konstanten.PI)  # Ausgabe: 3.14159
+print(konstanten.NAME)  # Ausgabe: Mathe-Modul
