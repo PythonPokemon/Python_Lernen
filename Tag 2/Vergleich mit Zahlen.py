f5ccb0864eca6dee6ec07749b83ae3e305dc6868
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-08-07 16:56:01 +0200

    update

---
+++ Tag 2/Vergleich mit Zahlen.py
@@ -0,0 +1,11 @@
+""" 
+Vergleich mit Zahlen
+"""
+
+zahl_string = "10"
+zahl_1 = 10
+zahl_2 = 10
+
+print(zahl_string == zahl_1)  # False (Typen unterschiedlich!)
+print(zahl_1 == zahl_2)  # True (beide sind 10)
+print(int(zahl_string) == zahl_1)  # True (nach Umwandlung)
---
+++ Tag 2/not in-Operator.py
@@ -0,0 +1,10 @@
+""" 
+in-Operator (Teil in String enthalten?)
+es wird geschaut ob ein Teilstring in einem String 'nicht' enthalten ist
+und gibt entsprechend True oder False zurück.
+
+"""
+sprache = "JavaScript"
+
+print("Java" not in sprache)  # False (weil es drin ist)
+print("Python" not in sprache)  # True
