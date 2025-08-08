34c5988080108c599d9a0823a4c5f890307bc491
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-08-08 13:03:15 +0200

    update

---
+++ Tag 3/Ausnahmen_Behandeln/syntax_error.py
@@ -0,0 +1,12 @@
+""" 
+# SyntaxError: Falscher Code-Aufbau (kann nicht mit try-except gefangen werden)
+# ❌ Dieser Code würde gar nicht starten:
+# if True print("Hallo")
+"""
+
+
+
+
+# ✅ Lösung: Code korrekt schreiben
+if True:
+    print("Hallo")
