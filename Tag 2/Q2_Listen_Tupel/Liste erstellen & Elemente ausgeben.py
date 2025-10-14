47586cb4bc66bad489036d6c99c6e074dac569ed
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-10-14 11:44:48 +0200

    neustes Update

--- Tag 2/Q2_Listen_Tupel/Liste erstellen & Elemente ausgeben.py
+++
@@ -1,5 +0,0 @@
-# 🧠 Aufgabe 6: Liste mit Obst
-# Gib das zweite Obst aus der Liste aus.
-
-obstListe = ["Apfel", "Banane", "Kirsche"]
-print(obstListe[1])  # Ausgabe: Banane
--- Tag 2/Q2_Listen_Tupel/Tupel erstellen & ausgeben.py
+++
@@ -1,6 +0,0 @@
-# 🧠 Aufgabe 7: Tupel mit Zahlen
-# Gib das letzte Element aus dem Tupel aus.
-
-# Index         0   1   2
-zahlenTupel = (10, 20, 30)
-print(zahlenTupel[2])
