db6e89e017297ff18a634b5cd0ddd13360491664
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-10-15 11:07:51 +0200

    neustes Update

--- Tag 3/Q6_Kodierungsstandards_ASCII_UNICODE_UTF-8/Zeichenkodierung.py
+++ Tag 3/Q6_Kodierungsstandards_ASCII_UNICODE_UTF-8/Zeichenkodierung.py
@@ -5,8 +5,20 @@ Ein Computer speichert Buchstaben als Zahlen.
 Jeder Buchstabe hat einen sogenannten Code Point.
 """
 
-print(ord("A"))   # → 65 (ASCII)
-print(chr(9786))  # → ☺ (Unicode)
+print(ord("A"))     # → 65 (ASCII)
+print(chr(65))      # 'A'  (Unicode)
+
 
 # Escape-Sequenz: z. B. \n (neue Zeile), \t (Tab)
 print("Hallo\nWelt!")  # Zeilenumbruch..............
+
+A = ord("A")
+B = ord("B")
+
+summe = A + B
+
+print("A entspricht: ", A)
+print("B entspricht: ", B)
+print("A:", A,  " + B:",  B, " = ", summe, "\n")
+
+
