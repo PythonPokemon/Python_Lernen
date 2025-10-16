c87f38541506b2bbb6eacca5c239fb7632a3214e
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-10-16 14:11:40 +0200

    neustes Update

--- Tag 1/Q2_Datentypen_Casting/Q2.1_Datentypen/Q2.1_Datentypen.py
+++ Tag 1/Q2_Datentypen_Casting/Q2.1_Datentypen/Q2.1_Datentypen.py
@@ -7,8 +7,8 @@ komplexe_zahl = 2 + 3j          # complex – komplexe Zahl mit Real- und Imagin
 text = "Hallo Welt"             # str – Zeichenkette
 wahrheitswert1 = True           # bool – Wahr
 wahrheitswert2 = False          # bool - Falsch
-liste = [1, 2, 3]               # list – veränderbare Liste
-tupel = (4, 5, 6)               # tuple – unveränderbare Liste
+liste = [1, 2, 3]               # list –  geordnete Sequenzen | veränderbare Liste
+tupel = (4, 5, 6)               # tuple – geordnete Sequenzen | unveränderbare Liste
 menge = {1, 2, 3}               # set – Menge ohne Duplikate
 dictionary = {"a": 1, "b": 2}   # dict – Schlüssel/Wert-Paare
 nichts = None                   # NoneType – "kein Wert"
@@ -19,10 +19,16 @@ print(type(kommazahl))          # <class 'float'>
 print(type(komplexe_zahl))      # <class 'complex'>
 print(type(text))               # <class 'str'>
 print(type(wahrheitswert1))     # <class 'bool'>
+print(type(wahrheitswert2))     # <class 'bool'>
 print(type(liste))              # <class 'list'>
 print(type(tupel))              # <class 'tuple'>
 print(type(menge))              # <class 'set'>
 print(type(dictionary))         # <class 'dict'>
 print(type(nichts))             # <class 'NoneType'>
+print("\n")
 
+# teste das!
+print("ab hier beginnt die Praxis:-)")
 
+irgendeineZahl = 12344
+print(type(irgendeineZahl))
