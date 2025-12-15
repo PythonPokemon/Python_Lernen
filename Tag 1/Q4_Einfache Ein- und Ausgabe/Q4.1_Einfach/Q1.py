d0e277eba66f5e62e4a4c3ec9a8140d99995db89
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-15 11:39:10 +0100

    neustes Update

--- Tag 1/Q4_Einfache Ein- und Ausgabe/Q4.1_Einfach/Q1.py
+++ Tag 1/Q4_Einfache Ein- und Ausgabe/Q4.1_Einfach/Q1.py
@@ -5,8 +5,8 @@
 name = input("Wie heißt du? ")
 print("Hallo, " + name + "! Willkommen im Python-Kurs.")
 
-print("I\'m R2D2")  # mit dem escabe charakter \ kann man hinterher sonderzeichen als literall einsetzen
+print("I\'m R2D2")  # mit dem escabe charakter \ kann man hinterher sonderzeichen als literal einsetzen
 print(R"I\'m R2D2") # Raw String | Das R sagt Python: „Nichts escapen, alles roh drucken.“
 
-print("abc", end="")            # 'end' == kein zeilenumbruch
+print("abc", end="")            # 'end' == kein zeilenumbruch, nachdem print befehl!
 print("defg", "hijk", sep="-")  # 'sep' == zwischen den strings, separate zeichen!
