b4ea8bbf7feb12ee9f4ba2aaa9442f3de391dceb
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-15 11:21:37 +0100

    neustes Update

--- Tag 1/Q4_Einfache Ein- und Ausgabe/Q4.1_Einfach/Q1.py
+++ Tag 1/Q4_Einfache Ein- und Ausgabe/Q4.1_Einfach/Q1.py
@@ -1,4 +1,5 @@
 # Aufgabe 1: Begrüßung
 # Frage den Benutzer nach seinem Namen und gib eine personalisierte Begrüßung aus.
+# inpu() == default type string
 name = input("Wie heißt du? ")
 print("Hallo, " + name + "! Willkommen im Python-Kurs.")
---
+++ testdatei.py
@@ -0,0 +1,2 @@
+print("abc", end="")            # 'end' == kein zeilenumbruch
+print("defg", "hijk", sep="-")  # 'sep' == zwischen den strings, separate zeichen!
