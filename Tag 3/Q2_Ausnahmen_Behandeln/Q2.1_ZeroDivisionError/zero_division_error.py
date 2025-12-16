7e4a06b4b77c9bb945c40d15917c44684254f4bc
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-16 16:30:29 +0100

    neustes Update

--- Tag 3/Q2_Ausnahmen_Behandeln/Q2.1_ZeroDivisionError/zero_division_error.py
+++ Tag 3/Q2_Ausnahmen_Behandeln/Q2.1_ZeroDivisionError/zero_division_error.py
@@ -6,4 +6,4 @@ Teilen durch Null geht nicht!
 try:
     ergebnis = 10 / 0   # teste 2, 5, 10 | wenn keine fehlerausgabe kommt, ist die Division durch Null nicht erfolgt           
 except ZeroDivisionError:
-    print("❌ Fehler: Durch Null darf man nicht teilen!")
+    print("❌ Fehler: Durch Null darf man nicht teilen!, dieser Fehler wird von ---> ", ZeroDivisionError, "<--- abgefangen.")
