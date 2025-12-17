57a1d82ceb834830e681f8a8186bb989c1d94515
Jakob Derzapf <jakob.derzapf@gfn.de>
2025-12-17 15:46:14 +0100

    neustes Update

--- Tag 3/Q2_Ausnahmen_Behandeln/Q3.0_Exception_Mix_Lösung.py
+++ Tag 3/Q2_Ausnahmen_Behandeln/Q3.0_Exception_Mix_Lösung.py
@@ -1,5 +1,6 @@
 """
 Versuch jetzt dein gelerntes wissen abzufragen indem du jede exception 1x triggerst
+die beschreibungen sind teilweise kontextbezogen zu dieser aufgabe!
 
 zum testen in den print() befehl einsetzten Lösung:
 -------------------------------------------------------------------------------------
@@ -7,19 +8,34 @@ Fehlerklassen       | in den print  |               beschreibung                
 -------------------------------------------------------------------------------------
 ValueError:         |   int("abc")  |   "abc" kann nicht in int umgewandelt werden  |
 -------------------------------------------------------------------------------------
-TypeError:          |   5 + "abc"   |    Zahl + String = nicht erlaubt              |
- ------------------------------------------------------------------------------------
+TypeError:          |   5 + "abc"   |   Zahl + String = nicht erlaubt               |
+-------------------------------------------------------------------------------------
 ZeroDivisionError:  |   10 / 0      |   Division durch Null nicht erlaubt           |
- ------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 SyntaxError:        | print(""      |   ungültiger Python-Ausdruck                  |
- ------------------------------------------------------------------------------------
-Exception as e:     |
+-------------------------------------------------------------------------------------
+AttributeError:     |
+-------------------------------------------------------------------------------------
+KeyError:           | dictionary[3] |   key existiert nicht, sondern [1] und [2]    |
+-------------------------------------------------------------------------------------
+NameError:          |      var      |    die variable existiert nicht               |
+-------------------------------------------------------------------------------------
+IndexError:         |   liste[5]    |   index 5 existiert nicht, nur: 0...4         |
+-------------------------------------------------------------------------------------
+ImportError:        |   import aaa  |   Modul konnte nicht importiert werden        |
+-------------------------------------------------------------------------------------
+Exception as e:     | keine speziele|   wenn eine spezielle klasse fehlt!           |
+-------------------------------------------------------------------------------------
 """
+#import aaa
 
 
+dictionary = { 1 : "eins", 2 : 3 }
+liste = [1, 2, 3, 4, 5]
+#var = "variable"
 
 try:
-    print(10/0)    # hier testen:-)
+    import aaa    # hier testen:-)
 
 except ValueError:
     print("❌ ValueError: Ungültiger Zahlenwert!")
@@ -37,13 +53,16 @@ except AttributeError:
     print("❌ AttributeError:")
 
 except KeyError:
-    print("❌ KeyError:")
+    print("❌ KeyError: key existiert nicht, sondern [1] oder [2]")
 
 except NameError:
-    print("❌ NameError:")
+     print("❌ NameError: die variable existiert nicht == evtl. hier ausgegraut!")
 
 except IndexError:
-    print("❌ IndexError:")
+    print("❌ IndexError: index 5 existiert nicht, die Indizes sind jeweils von: 0...4")
+
+except ImportError:
+    print("❌ Fehler: Dieses Modul konnte nicht importiert werden!")
 
 except Exception as e:
-    print("⚠️ Ein anderer Fehler ist aufgetreten:", type(e).__name__)
+    print("⚠️  Allgemeine Fehlerbehandlung: Ein anderer Fehler ist aufgetreten, der hier nicht definiert wurde:", type(e).__name__)
