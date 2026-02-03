cc3f363fd36be3e94abe432a0fb77820af89fcdd
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-03 09:24:55 +0100

    neustes Update

--- TN_Test.py
+++ TN_Test.py
@@ -1,11 +1,3 @@
-import time
-
-count=0
-while True:
-    try:
-        count += 1
-        time.sleep(0.0000001)
-        print(count)
-    except KeyboardInterrupt:
-        print(f"Deine Zahl ist {count}.")
-        break
+"""
+test
+"""
--- Tag 1/Q3_Operatoren/Q3.1_Zuweisungsoperatoren/Q3.1.1_Einfach/Q1.py
+++ Tag 1/Q3_Operatoren/Q3.1_Zuweisungsoperatoren/Q3.1.1_Einfach/Q1.py
@@ -10,18 +10,25 @@ Aufgabe 2: Alle Operatoren einmal durchspielen.
 """
 x = 5
 print(x)  # Ausgabe: 5
+
 x += 2    # x = x + 2 → 7
 print(x)  # Ausgabe: 7
+
 x -= 1    # x = x - 1 → 6
 print(x)  # Ausgabe: 6
+
 x *= 3    # x = x * 3 → 18
 print(x)  # Ausgabe: 18
+
 x /= 2    # x = x / 2 → 9.0
 print(x)  # Ausgabe: 9.0
+
 x //= 2   # x = x // 2 → 4.0
 print(x)  # Ausgabe: 4.0
+
 x %= 3    # x = x % 3 → 1.0
 print(x)  # Ausgabe: 1.0
+
 x **= 2   # x = x ** 2 → 1.0
 print(x)  # Ausgabe: 1.0
     
@@ -45,5 +52,3 @@ y //= 3   # 3
 y %= 2    # 1
 y **= 3   # 1
 print("Endwert von y:", y)
-
-
