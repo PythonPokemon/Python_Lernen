ae896ed1c4f4498fe666fdfba539caa7f3158c2e
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-03 11:08:59 +0100

    neustes Update

--- Tag 4/Q5_konstanten.py
+++ Tag 4/Q5_konstanten.py
@@ -1,10 +1,15 @@
 """ 
 Wie schreibt man ein benutzerdefiniertes Modul mit Konstanten?
 
-Konstanten können nachdem initialisieren nicht mehr verändert werden!
+Konstanten können nachdem initialisieren nicht mehr verändert werden!?
+
+in Python gibt es keine echten Konstanten sondern nur Konventionen.!!!
 """
 
 PI = 3.14159
 NAME = "Mathe-Modul"    # konstante wierd in Großbuchstaben geschrieben
 
+print(PI)
 
+PI = 3.11111
+print(PI)  # Der Wert von PI wurde verändert, obwohl es eine Konstante sein soll!
