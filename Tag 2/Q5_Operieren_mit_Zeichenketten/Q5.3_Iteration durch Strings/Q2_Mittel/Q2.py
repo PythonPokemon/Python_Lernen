5d24664eb5a0202c89a149426a2d6d291d62f693
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-05 14:21:39 +0100

    neustes Update

---
+++ Tag 2/Q5_Operieren_mit_Zeichenketten/Q5.3_Iteration durch Strings/Q2_Mittel/Q2.py
@@ -0,0 +1,9 @@
+""" 
+Iteration durch Strings (Zeichenweise durchgehen)
+mit slicing [start:stop:step]
+"""
+
+zahlenListe = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   # in der veraiable 'name' wird eine Zeichenkette "Anna" gespeichert
+
+for i in zahlenListe[0:11:2]:   # in der Kontroll_Variable variable 'i' werden die Werte während der Iteration zwischengespeichert die, 
+    print(i)                    # Ausgabe jedes einzelnen wertes die in der Kontroll_Variable 'i' zwischen gespeichert wurden
