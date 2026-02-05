ccac56af70c1fcfdc467eb68bf4932406d4e3e8f
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-05 14:01:29 +0100

    neustes Update

--- Tag 2/Q5_Operieren_mit_Zeichenketten/Q5.2_Indizierung_Slicing_Immutability/Zeichenketten indizieren und slicen.py
+++ Tag 2/Q5_Operieren_mit_Zeichenketten/Q5.2_Indizierung_Slicing_Immutability/Zeichenketten indizieren und slicen.py
@@ -4,3 +4,18 @@
 wort = "Python"
 print(wort[0])      # Erster Buchstabe
 print(wort[0:3])    # Erste drei Buchstaben
+
+# umgekehrt
+print(wort[-1])     # Letzter Buchstabe
+
+# -----------------------------------------------------------------
+
+liste = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(liste[0])     # Erster Wert
+print(liste[0:3])   # Erste drei Werte
+print(liste[2:11:2])  # Werte von Index 2 bis 10 mit Schrittweite 2
+
+# umgekehrt
+print(wort[-1])     # Letzter Buchstabe
+print(wort[-3:])    # Letzte drei Buchstaben
+print(liste[-1])    # Letzter Wert
