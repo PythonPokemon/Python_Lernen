e31f7cb240f2e8b039c019619964341367a36dc6
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-05 17:50:14 +0100

    neustes Update

---
+++ Tag 3/Q1_Funktionen_Rekursion_return_none/Q1.1_Funktionen/Q2_Mittel/Q1.py
@@ -0,0 +1,14 @@
+"""
+📌 Was ist eine Funktion?
+Eine Funktion ist ein kleines Programm im Programm, das man mehrfach verwenden kann.
+"""
+# Bsp. 1
+
+def addieren():
+    zahl1 = 5
+    zahl2 = 10
+    ergebnis = zahl1 + zahl2
+    print(ergebnis)
+
+addieren()  # Aufruf der Funktion
+
---
+++ Tag 3/Q1_Funktionen_Rekursion_return_none/Q1.1_Funktionen/Q3_Schwer/Q1.py
@@ -0,0 +1,10 @@
+# Funktion mit Parametern und Rückgabewert
+def addieren(a, b):
+    ergebnis = a + b
+    return ergebnis
+
+x = 7
+y = 3
+
+summe = addieren(x, y)
+print("Ergebnis:", summe * 2)
