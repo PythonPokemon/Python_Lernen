7af4ed89be5babdfd0e382ff040d7e0e0c113673
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-05 17:43:56 +0100

    neustes Update

--- Tag 3/Q1_Funktionen_Rekursion_return_none/Q1.2_Rekursion/Rekursion.py
+++ Tag 3/Q1_Funktionen_Rekursion_return_none/Q1.2_Rekursion/Rekursion.py
@@ -2,7 +2,13 @@
 📌 Was ist Rekursion?
 Eine Funktion, die sich selbst aufruft. Wird oft in Mathe benutzt.
 
-💡 Beispiel: Fakultät (5! = 5×4×3×2×1)
+bspl.
+fakultaet(4)
+= 4 * fakultaet(3)
+= 4 * 3 * fakultaet(2)
+= 4 * 3 * 2 * fakultaet(1)
+= 4 * 3 * 2 * 1
+= 24
 """
 
 def fakultaet(n):
@@ -10,4 +16,4 @@ def fakultaet(n):
         return 1
     return n * fakultaet(n - 1)
 
-print(fakultaet(5))  # Ergebnis: 120
+print(fakultaet(4))  # Ergebnis: 120
