920a7a52d9a99e2bd40e8efc8c48de751fce1c15
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-05 11:10:18 +0100

    neustes Update

---
+++ Tag 2/Q4_Eingebaute_String_Methoden/Q4.8_rfind()/Q2_Mittel/Q1.py
@@ -0,0 +1,19 @@
+"""
+❓ Aufgabe:
+
+Was wird ausgegeben?
+Erkläre kurz, warum.
+
+
+P r o g r a m m i e r e n   l e r n e n
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+"""
+
+
+text = "Programmieren lernen"
+pos1 = text.find("e")
+pos2 = text.rfind("e")
+
+print(pos1)
+print(pos2)
