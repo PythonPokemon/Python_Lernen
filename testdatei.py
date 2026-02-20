46514932a5491ed99a4cec34f833e99ca4a6c218
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-02-20 09:58:05 +0100

    neustes Update

--- testdatei.py
+++ testdatei.py
@@ -1,19 +1,3 @@
 # Testdatei für die Wiederholung von Themen
 # Step-by-Step
 
-# variante 1 | wenn man schon im vorfeld variablen mit werten hat!
-zahl = 10   #
-
-print(zahl)
-print(type(zahl))
-print("\n")
-print(float(zahl))
-
-kommazahl = 3.14
-print(kommazahl)
-print(type(kommazahl))
-print(int(kommazahl))
-
-# variante 2 | direktumwandlung
-
-print(int(3.99))
