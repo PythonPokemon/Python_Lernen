4edeb06457ba9518adaceb25c0004750d9542819
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-03-02 13:50:04 +0100

    neustes Update

--- Tag 2/Q3_Komplexe_Listen_Comprehension_Dictionaries/Q3.1_Dictionarys/Q3.1.8_for_i_index_Dictionary.py
+++ Tag 2/Q3_Komplexe_Listen_Comprehension_Dictionaries/Q3.1_Dictionarys/Q3.1.8_for_i_index_Dictionary.py
@@ -1,14 +1,17 @@
 
 # gibt die werte des dictionarys als variablen aus.
 # bestimmt mit [0] den value-Index
-dct = {}
+
+dct = {}    # dictionary leer
 
 # Tupel
 # Index:    0  1
 #           |  |
-dct['1'] = (1, 2)   # index [0] = key '1', value = (1, 2)
-dct['2'] = (2, 1)   # index [1] = key '2', value = (2, 1)
-dct['3'] = (3, 0)   # index [2] = key '3', value = (3, 0)
+dct['1'] = (1, 2)   # key '1', value = (1, 2)
+dct['2'] = (2, 1)   # key '2', value = (2, 1)
+dct['3'] = (3, 0)   # key '3', value = (3, 0)
+
+print(dct)  # dictionary befüllt: {'1': (1, 2), '2': (2, 1), '3': (3, 0)}
 
 for x in dct.keys():
-    print(dct[x][0], end="")
+    print(dct[x][0], end="")    # mit , end=""  sagt man lediglich das kein zeilenumbruch stattfinden soll!
