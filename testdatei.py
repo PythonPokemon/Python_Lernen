e90d4abc91ef482c14bb1d7dc7f5ec7c6a88abe3
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-03-04 15:14:45 +0100

    neustes Update

--- testdatei.py
+++ testdatei.py
@@ -10,21 +10,21 @@ print("-----")
 
 vals = [0, 1, 2]
 vals.insert(0, 1)
-print(vals)
+print(vals)             # Ausgabe?
 del vals[1]
-print(vals)
+print(vals)             # Ausgabe?
 
 print("---------------------------------------------------------------")
 #index    -4  -3 -2 -1
 my_list = [1, 2, 3, 4]
-print(my_list[-3:-2]) # 2
+print(my_list[-3:-2])   # Ausgabe?
 
 print("---------------------------------------------------------------")
 z = 10
 y = 0
 x = y < z and z > y 
 
-print(x)
+print(x)                # Ausgabe?
 
 print("---------------------------------------------------------------")
 a = 1
@@ -34,15 +34,113 @@ c = a & b   # and
 d = a | b   # bitwise or
 e = a ^ b   # XOR exclusiv or
 
-print(c + d + e)    # 0 + 1 + 1
+print(c + d + e)        # Ausgabe?
 
 print("---------------------------------------------------------------")
 
 z = y = x = 1
-print(x, y, z, sep='*')
+print(x, y, z, sep='*') # Ausgabe?
 
 print("---------------------------------------------------------------")
 
 variable = 1, 2, 3, 456, 8888, 1.12, [12], {1333}, ("hgv")
 
-print(variable)
+print(variable)         # daraus wird eine tupel
+
+print("---------------------------------------------------------------, Modul 3 Quiz")
+#index revevrse
+#         -3  -2  -1
+my_list = [3, 1, -1]
+my_list[-1] = my_list[-2]   # der wert -2 ist 1 und dieser wird der liste übergeben und diese setzt ihn wider auf einen neuen index
+print(my_list)          # Ausgabe?
+
+print("---------------------------------------------------------------")
+
+nums = []
+vals = nums
+
+vals.append(1)
+print(nums)
+print(vals)
+print(nums == vals) # werte gleich?
+print(nums is vals) # adresse gleich? | Referenz zum gleichen objekt?
+print(id(nums))
+print(id(vals))
+
+print("---------------------------------------------------------------")
+nums2 = []
+vals2 = nums2[:]
+
+vals2.append(1)
+print(nums2)
+print(vals2)
+print(nums2 == vals2) # werte gleich?
+print(nums2 is vals2) # adresse gleich? | Referenz zum gleichen objekt?
+print(id(nums2))
+print(id(vals2))
+
+print("---------------------------------------------------------------")
+
+my_list3 = [0 for i in range(1, 3)] # inkl: 1, excl: 3
+print(my_list3)                     # Ausgabe?
+
+print("---------------------------------------------------------------")
+
+my_list4 = [0, 1, 2, 3]
+x = 1
+for element in my_list4:
+    x *= element
+print(x)                            # Ausgabe?
+
+print("--------------------------------------------------------------- Modultest 3")
+
+var5 = 0
+while var5 < 6:
+    var5 += 1
+    if var5 % 2 == 0:
+        continue
+    print("#")                      # Ausgabe: 3 sterne
+
+print("---------------------------------------------------------------")
+
+nums6 = [1, 2, 3]
+vals6 = nums6 [-1:-2]
+
+print(vals6)                        #
+
+print("---------------------------------------------------------------")
+
+for i in range(1):                  # 1 ist exclusiv
+    print("#")
+else:
+    print("#")                      # Ausgabe?
+
+print("---------------------------------------------------------------")
+
+my_list7 = [[0, 1, 2, 3] for i in range (2)]    #  ist exclusiv
+print(my_list7[1][0])               # Ausgabe?
+
+print("---------------------------------------------------------------")
+print("kompliziert")
+my_list8 = [1, 2, 3]
+
+for v in range (len(my_list8)):     #range(3)
+    my_list8.insert(1, my_list8[v])
+print(len(my_list8))
+
+print("---------------------------------------------------------------")
+
+my_list9 = [1, 2, 3]
+my_list10 = []
+
+for v in my_list9:
+    my_list10.insert(0, v)
+print(my_list10)            # insert schiebt die elemente nach rechts!
+
+print("---------------------------------------------------------------")
+
+dd = {"1": "0", "0": "1"}
+
+for x in dd.values():
+    print(x, end="")
+    
