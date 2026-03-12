c9a51b4b5a8bd10a24fd2d54d0ad0f5877a55eb3
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-03-12 16:15:25 +0100

    neustes Update

---
+++ Tag 1/Q3_Operatoren/Q3.6_and_binär.py
@@ -0,0 +1,25 @@
+
+
+i = 6
+
+j = 5
+ 
+log1 = i and j  # bedeutte alles über 0 ist true, also wird das letztere ausgegeben
+print(log1)
+
+"""
+fiktives bsp. da viablen freie werte zugewiesen wurden
+                2er potenz
+                8 4 2 1
+Binär i ==6:    0 | | 0
+Binär j ==5:    0 | 0 |
+-----------------------
+Ergebnis:       0 | 0 0     # für & also: 0 + 4 + 0 + 0 == 4
+Ergebnis:       0 | | |     # für | or, da auch 1 und 1 richtig ist, also: 0 + 4 + 2 + 1 == 7
+"""
+log2 = i & j    # bin#r operation
+
+print(log2)
+ 
+log2 = i | j 
+print(log2)
