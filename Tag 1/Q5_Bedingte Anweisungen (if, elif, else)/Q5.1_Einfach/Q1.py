bdad7680f28543dd50f4954e5c1b39bdebdc378f
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-04-28 14:45:00 +0200

    neustes Update

--- Tag 1/Q5_Bedingte Anweisungen (if, elif, else)/Q5.1_Einfach/Q1.py
+++ Tag 1/Q5_Bedingte Anweisungen (if, elif, else)/Q5.1_Einfach/Q1.py
@@ -10,6 +10,6 @@ zahl = int(input("Gib eine Zahl ein: "))
 if zahl > 0:
     print("Die Zahl ist positiv.")  # trifft zu, wenn Zahl größer als 0 ist
 elif zahl < 0:
-    print("Die Zahl ist negativ.")  # trifft zu, wenn Zahl kleiner als 0 ist
+    print("Die Zahl ist negativ.")  # trifft zu, wenn Zahl kleiner als 0 ist, bsp. negiert -1
 else:
     print("Die Zahl ist null.")     # trifft zu, wenn Zahl genau 0 ist
