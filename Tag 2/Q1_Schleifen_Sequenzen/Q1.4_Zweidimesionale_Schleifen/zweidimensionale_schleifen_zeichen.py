71a9b9a4fa0b2e32fe8717af8fe58cae1d2c8bf9
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-05-05 11:35:42 +0200

    neustes Update

--- Tag 2/Q1_Schleifen_Sequenzen/Q1.4_Zweidimesionale_Schleifen/zweidimensionale_schleifen_zeichen.py
+++ Tag 2/Q1_Schleifen_Sequenzen/Q1.4_Zweidimesionale_Schleifen/zweidimensionale_schleifen_zeichen.py
@@ -2,19 +2,25 @@
 Von oben nach unten: Zeilenindex → äußere Schleife (<i>) → Regale
 Von links nach rechts: Spaltenindex → innere Schleife (<j>) → Fächer
 
-|   Zeile <i> (Regal)   |   Spalte <j> / Fach 0  |   Spalte <j> / Fach 1   |
+|   Zeile <i> (Regal)   |   Spalte <j> | Fach    |   Spalte <j> | Fach     |
 |-----------------------|------------------------|-------------------------|
-|   [0] (= 1. Regal)    | Hund [0][0]            | Katze [0][1]            |
+|   [0]       (Zeile)   | Hund [0][0]  | Fach 0  | Katze [0][1] | Fach 1   |
 |-----------------------|------------------------|-------------------------|
-|   [1] (= 2. Regal)    | Maus [1][0]            | Vogel [1][1]            |
+|   [1]       (Zeile)   | Maus [1][0]  | Fach 1  | Vogel [1][1] | Fach 1   |
 |-----------------------|------------------------|-------------------------|
 """
 
 tiere = [
-    ["Hund", "Katze"],   
-    ["Maus", "Vogel"]    
-]
+        ["Hund", "Katze"],   
+        ["Maus", "Vogel"]
+        ]
 
-for i in range(len(tiere)):            # äußere Schleife → Regale
-    for j in range(len(tiere[i])):     # innere Schleife → Fächer
-        print(f"Regal {i}, Fach {j} → {tiere[i][j]}")
+for i in range(len(tiere)):            # äußere Schleife → Regale/Zeile
+    for j in range(len(tiere[i])):     # innere Schleife → Regal/SPalteFächer
+        print(f"In der Regal Zeile: {i}, im Fach {j} befindet sich → {tiere[i][j]}")
+
+# bestimmung der ausgabe durch index: ist 'zeile' [0] und 'spalte' [0]
+print(tiere[0][0])  # ?
+print(tiere[0][1])  # ?
+print(tiere[1][0])  # ?
+print(tiere[1][1])  # ?
