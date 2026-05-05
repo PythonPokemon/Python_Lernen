f7b6459dc7aa7f33fdce707287c93396fb08afe9
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-05-05 11:43:41 +0200

    neustes Update

--- Tag 2/Q2_Listen_Tupel/Q2.1_Listen/Q5_insert()_Liste[]_for_i.py
+++ Tag 2/Q2_Listen_Tupel/Q2.1_Listen/Q5_insert()_Liste[]_for_i.py
@@ -1,9 +1,12 @@
-
+"""
+.insert(startindex, wert) parameter angabe
+bei .insert() werden die elemente imme nach rechts verschoben
+"""
 
 def my_list(n):
-    list = []
+    list = [55, 77]
     for i in range(0,n):
-        list.insert(0,i)    #  bei insert werden die elemente imme nach rechts verschoben
+        list.insert(1,i)    #  startindex == 1, hier werden die neuen werte insertet !
     return list
     
 print(my_list(3))
