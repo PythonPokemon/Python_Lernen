1309b2d2909e3cae6a219900800e32eb6fe9152f
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-05-05 13:50:53 +0200

    neustes Update

--- Tag 2/Q4_Eingebaute_String_Methoden/Q4.8_rfind()/Q1_Einfach/Q1.py
+++ Tag 2/Q4_Eingebaute_String_Methoden/Q4.8_rfind()/Q1_Einfach/Q1.py
@@ -1,6 +1,4 @@
 """ 
-Beispiel: sorted() - Gibt eine neue sortierte Liste zurück (Original bleibt erhalten)
-
 
 | Zeichen | R | e | g | e | n | s | c | h | a | u | e  | r  |
 | Index   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 |
