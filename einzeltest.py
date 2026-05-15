5a553ef7b4b220c23acb4dbe79e1280062de1391
Jakob Derzapf <jakob.derzapf@gfn.de>
2026-05-15 16:32:26 +0200

    neustes Update

--- einzeltest.py
+++ einzeltest.py
@@ -1,6 +1,4 @@
-# index;   0  1  2
-# count 3
-my_list = [9, 7, 8]
-for v in range(len(my_list)):
-    my_list.insert(1, my_list[v])
-print(my_list)
+var = (1)
+
+print(var)
+print(type(var))    # tupel (1,)
--- zum_ueben.py
+++ zum_ueben.py
@@ -260,7 +260,12 @@ for i in sorted(dictionary.keys()):
 
 print("---------------------------------------------------------------")
 
-
+# index;   0  1  2
+# count 3
+my_list = [9, 7, 8]
+for v in range(len(my_list)):
+    my_list.insert(1, my_list[v])
+print(my_list)
 
 print("---------------------------------------------------------------")
 
